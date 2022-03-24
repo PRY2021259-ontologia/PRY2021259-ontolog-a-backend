748147a2f9a2a03dccb22114d3f7ec11d7212858
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-03-24 13:35:03 -0500

    initial commit

---
+++ Ontologia.API/Domain/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Ontologia.API.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
