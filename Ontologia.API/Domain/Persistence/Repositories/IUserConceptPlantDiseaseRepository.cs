beb754d3a9351eb9b63ea07b9550bfea7d54d138
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-09 19:44:34 -0500

    feat: add uConceptPDisease repository

---
+++ Ontologia.API/Domain/Persistence/Repositories/IUserConceptPlantDiseaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using Ontologia.API.Domain.Models;
+
+namespace Ontologia.API.Domain.Persistence.Repositories
+{
+    public interface IUserConceptPlantDiseaseRepository
+    {
+        Task<IEnumerable<UserConceptPlantDisease>> ListAsync();
+        Task<IEnumerable<UserConceptPlantDisease>> ListByUserConceptIdAsync(Guid userConceptId);
+        Task<IEnumerable<UserConceptPlantDisease>> ListByPlantDiseaseIdAsync(Guid plantDiseaseId);
+        Task<IEnumerable<UserConceptPlantDisease>> ListByUserIdAndTutorIdAsync(Guid userId, Guid tutorId);
+        Task<UserSession> FindByUserIdAndSessionId(Guid userId, Guid sessionId);
+        Task AddAsync(UserSession userSession);
+        void Remove(UserSession userSession);
+        Task AssignUserSession(Guid userId, Guid sessionId);
+        Task UnassignUserSession(Guid userId, Guid sessionId);
+    }
+}
