cb6f8ee617cc8d569512e2de4b1de534838eb6d5
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-09 19:51:27 -0500

    feat: add userConceptPlantDisease service

---
+++ Ontologia.API/Domain/Services/IUserConceptPlantDiseaseService.cs
@@ -0,0 +1,14 @@
+﻿using Ontologia.API.Domain.Models;
+
+namespace Ontologia.API.Domain.Services
+{
+    public interface IUserConceptPlantDiseaseService
+    {
+        Task<IEnumerable<UserConceptPlantDisease>> ListAsync();
+        Task<IEnumerable<UserConceptPlantDisease>> ListByUserConceptIdAsync(Guid userConceptId);
+        Task<IEnumerable<UserConceptPlantDisease>> ListByPlantDiseaseIdAsync(Guid plantDiseaseId);
+        Task<IEnumerable<UserConceptPlantDisease>> ListByUserConceptIdAndPlantDiseaseIdAsync(Guid userConceptId, Guid plantDiseaseId);
+        Task<UserConceptPlantDisease> AssignUserConceptPlantDiseaseAsync(Guid userConceptId, Guid plantDiseaseId);
+        Task<UserConceptPlantDisease> UnassignUserConceptPlantDiseaseAsync(Guid userConceptId, Guid plantDiseaseId);
+    }
+}
