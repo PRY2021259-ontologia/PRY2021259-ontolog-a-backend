df29a8419bd6b3bf36c48ed9f0cba2e7c3c900b0
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-10 00:41:13 -0500

    feat: add settings for user authentication

---
+++ Ontologia.API/Services/UserAuthService.cs
@@ -0,0 +1,24 @@
+﻿
+using AutoMapper;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using StudyDesck.API.Domain.Models;
+using StudyDesck.API.Domain.Persistence.Repositories;
+using StudyDesck.API.Domain.Services;
+using StudyDesck.API.Domain.Services.Comunications;
+using StudyDesck.API.Settings;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using BCryptNet = BCrypt.Net;
+
+namespace Ontologia.API.Services
+{
+    public class UserAuthService
+    {
+    }
+}
