86fb96a45b7a48b84e104e0f82807a16257308ab
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-18 17:54:08 -0500

    feat: add userLoginService interface

---
+++ Ontologia.API/Domain/Services/Communications/UserLoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using Ontologia.API.Domain.Models;
+
+namespace Ontologia.API.Domain.Services.Communications
+{
+    public class UserLoginResponse : BaseResponse<UserLogin>
+    {
+        public UserLoginResponse(UserLogin resource) : base(resource)
+        {
+        }
+
+        public UserLoginResponse(string message) : base(message)
+        {
+        }
+    }
+}
