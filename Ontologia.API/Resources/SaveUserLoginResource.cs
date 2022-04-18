22e21d5f4af98fe5b6788600e61f91c73a3d2e89
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-18 18:32:14 -0500

    feat: add userLogin resources

---
+++ Ontologia.API/Resources/SaveUserLoginResource.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ontologia.API.Resources
+{
+    public class SaveUserLoginResource
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public bool isActive { get; set; }
+    }
+}
