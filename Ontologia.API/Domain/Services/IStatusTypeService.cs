56e1dff83383308ac9965ecd72bfb85b4aeb06b3
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-19 11:18:40 -0500

    feat: add statusType service impl

--- Ontologia.API/Domain/Services/IStatusTypeService.cs
+++ Ontologia.API/Domain/Services/IStatusTypeService.cs
@@ -8,8 +8,8 @@ namespace Ontologia.API.Domain.Services
         // General Methods
         Task<IEnumerable<StatusType>> ListAsync();
         Task<StatusTypeResponse> GetByIdAsync(Guid id);
-        Task<StatusTypeResponse> SaveAsync(User user);
-        Task<StatusTypeResponse> UpdateAsync(Guid id, User user);
+        Task<StatusTypeResponse> SaveAsync(StatusType statusType);
+        Task<StatusTypeResponse> UpdateAsync(Guid id, StatusType statusType);
         Task<StatusTypeResponse> DeleteAsync(Guid id);
     }
 }
