b3e303f3f7a4ab8342ada7b6807831659f84629c
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-19 10:27:46 -0500

    feat: add suggestionStatus model

---
+++ Ontologia.API/Domain/Models/SuggestionStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace Ontologia.API.Domain.Models
+{
+    public class SuggestionStatus
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Usl { get; set; }
+        public bool IsProcessed { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+
+        // Relationship with UserSuggestion Entity
+        public Guid? UserSuggestionId { get; set; }
+        public UserSuggestion? UserSuggestion { get; set; }
+    }
+}
