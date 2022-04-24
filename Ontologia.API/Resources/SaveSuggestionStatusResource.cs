31aa3a25379c2c6c14a171f3897d82f982e1bf0c
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-24 15:21:55 -0500

    feat: add suggestionStatus resources

---
+++ Ontologia.API/Resources/SaveSuggestionStatusResource.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ontologia.API.Resources
+{
+    public class SaveSuggestionStatusResource
+    {
+        [Required]
+        public string SuggestionStatusTitle { get; set; }
+        [Required]
+        public string SuggestionStatusDescription { get; set; }
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public bool IsProcessed { get; set; }
+    }
+}
