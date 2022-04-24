9fb6bfdbd78af39de46b1c803eb1b75e8ba2ad6e
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-04-24 15:12:11 -0500

    feat: add suggestionStatusService

---
+++ Ontologia.API/Domain/Services/ISuggestionStatusService.cs
@@ -0,0 +1,25 @@
+﻿using Ontologia.API.Domain.Models;
+using Ontologia.API.Domain.Services.Communications;
+
+namespace Ontologia.API.Domain.Services
+{
+    public interface ISuggestionStatusService
+    {
+        // General Methods
+        Task<SuggestionStatusResponse> SaveAsync(SuggestionStatus SuggestionStatus);
+        Task<IEnumerable<SuggestionStatus>> ListAsync();
+        Task<SuggestionStatusResponse> GetById(Guid suggestionStatusId);
+        Task<SuggestionStatusResponse> Update(Guid suggestionStatusId, SuggestionStatus suggestionStatus);
+        Task<SuggestionStatusResponse> Delete(Guid suggestionStatusId);
+
+        // Methods for StatusType Entity
+        Task<IEnumerable<SuggestionStatus>> ListByStatusTypeId(Guid statusTypeId);
+        Task<SuggestionStatusResponse> AssingSuggestionStatusToStatusType(Guid statusTypeId, Guid suggestionStatusId);
+        Task<SuggestionStatusResponse> UnassingSuggestionStatusToStatusType(Guid statusTypeId, Guid suggestionStatusId);
+
+        // Methods for UserSuggestion Entity
+        Task<IEnumerable<SuggestionStatus>> ListByUserSuggestionId(Guid userSuggestionId);
+        Task<SuggestionStatusResponse> AssingSuggestionStatusToUserSuggestion(Guid userSuggestionId, Guid suggestionStatusId);
+        Task<SuggestionStatusResponse> UnassingSuggestionStatusToUserSuggestion(Guid userSuggestionId, Guid suggestionStatusId);
+    }
+}
